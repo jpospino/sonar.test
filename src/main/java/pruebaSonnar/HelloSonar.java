17cd4f2878c997e3cd58ce0ac6275ae2f16e6749
juan.ospino <jp.ospino@uniandes.edu.co>
2020-08-03 14:21:19 -0500

    first commit

---
+++ src/main/java/pruebaSonnar/HelloSonar.java
@@ -0,0 +1,13 @@
+package pruebaSonnar;
+
+import java.io.Console;
+
+public class HelloSonar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello sonar!!!!");
+
+	}
+
+}
